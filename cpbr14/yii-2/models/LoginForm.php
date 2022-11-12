bc61b6128557b1c552929f4e1cb7412df5cb6857
Matheus Evangelista Morais <thtmorais@hotmail.com>
2022-11-12 18:18:23 -0300

    .gitlab-ci.yml

--- cpbr14/yii-2/models/LoginForm.php
+++ cpbr14/yii-2/models/LoginForm.php
@@ -59,9 +59,6 @@ class LoginForm extends Model
      */
     public function login()
     {
-        if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
-        }
         return false;
     }
 
